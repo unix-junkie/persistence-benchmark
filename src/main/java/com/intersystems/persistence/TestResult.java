3a10b6784362fce7b3a747dead8436aeff9c9996
Andrey Shcheglov <andrey.shcheglov@intersystems.com>
2013-09-18 13:22:50 +0400

    MAJOR: enhanced to support error checking

--- src/main/java/com/intersystems/persistence/TestResult.java
+++ src/main/java/com/intersystems/persistence/TestResult.java
@@ -7,15 +7,47 @@ package com.intersystems.persistence;
  * @author Andrey Shcheglov &lt;mailto:andrey.shcheglov@intersystems.com&gt;
  */
 public final class TestResult {
+	/**
+	 * @author Andrey Shcheglov &lt;mailto:andrey.shcheglov@intersystems.com&gt;
+	 */
+	private static enum TestStatus {
+		READY,
+		SUCCESS,
+		FAILURE,
+	}
+
+	private final TestStatus status;
+
+	private final String message;
+
 	private final long numEvents;
 
 	private final double seconds;
+
+	public TestResult() {
+		this.status = TestStatus.READY;
+		this.message = this.status.name();
+		this.numEvents = 0;
+		this.seconds = 0;
+	}
+
+	/**
+	 * @param t
+	 */
+	public TestResult(final Throwable t) {
+		this.status = TestStatus.FAILURE;
+		this.message = t.getMessage();
+		this.numEvents = 0;
+		this.seconds = 0;
+	}
 
 	/**
 	 * @param numEvents
 	 * @param timeMillis
 	 */
 	public TestResult(final long numEvents, final long timeMillis) {
+		this.status = TestStatus.SUCCESS;
+		this.message = null;
 		this.numEvents = numEvents;
 		this.seconds = timeMillis / 1e3;
 	}
@@ -25,7 +57,21 @@ public final class TestResult {
 	}
 
 	public String getMessage() {
-		return this.numEvents + " event(s) in " + this.seconds + " second(s) (" + this.getRateEps() + " eps)";
+		switch (this.status) {
+		case SUCCESS:
+			return this.numEvents + " event(s) in " + this.seconds + " second(s) (" + this.getRateEps() + " eps)";
+		default:
+			return this.message;
+		}
+	}
+
+	/**
+	 * Either ready or success.
+	 *
+	 * @return
+	 */
+	public boolean isSuccessful() {
+		return this.status != TestStatus.FAILURE;
 	}
 
 	/**
